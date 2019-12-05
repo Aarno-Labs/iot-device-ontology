57baa42aab03b61eaf5d549774adcfbaf63e9191
Michael Gordon <mgordon@aarno-labs.com>
2019-12-05 11:19:09 -0500

    add software fields to host, and create a network test

---
+++ tests/test_network.py
@@ -0,0 +1,11 @@
+# tests/test_network.py
+
+import json
+from support.assertions import assert_valid_schema
+
+
+def test_get_network():
+    with open('tests/files/test-network.json') as json_file:
+        json_data = json.load(json_file)
+
+        assert_valid_schema(json_data, 'network.json.schema')
