7a26c9071b11856543b57912d31c574ce79b2ad6
Michael Gordon <mgordon@aarno-labs.com>
2019-12-11 10:55:36 -0500

    adding schemas and tests for more entities

---
+++ tests/test_isatype.py
@@ -0,0 +1,11 @@
+# tests/test_isatypeVersion.py
+
+import json
+from support.assertions import assert_valid_schema
+
+
+def test_get_isatype():
+    with open('tests/files/test-isatype.json') as json_file:
+        json_data = json.load(json_file)
+
+        assert_valid_schema(json_data, 'isatype.json.schema')
---
+++ tests/test_service.py
@@ -0,0 +1,11 @@
+# tests/test_serviceVersion.py
+
+import json
+from support.assertions import assert_valid_schema
+
+
+def test_get_service():
+    with open('tests/files/test-service.json') as json_file:
+        json_data = json.load(json_file)
+
+        assert_valid_schema(json_data, 'service.json.schema')
