90ecbd3fb2fcdf304dfb2514891041a880ebfafe
Michael Gordon <mgordon@aarno-labs.com>
2020-03-03 13:07:00 -0500

    schema for neutralizations; expect structured neutralizations in operation plan; check syntax of policy statements

---
+++ tests/test_neutralization.py
@@ -0,0 +1,11 @@
+# tests/test_network-based.py
+
+import json
+from support.assertions import assert_valid_schema
+
+
+def test_get_network_based():
+    with open('tests/files/test_network-based.json') as json_file:
+        json_data = json.load(json_file)
+
+        assert_valid_schema(json_data, 'operations/neutralization.json.schema')
