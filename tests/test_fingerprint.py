fa1c45c33081ad5f96353000093e8b56396f3167
kyle kubis <kyle.kubis@twosixlabs.com>
2020-10-29 08:27:18 -0400

    added most basic example

--- tests/test_fingerprint.py
+++ tests/test_fingerprint.py
@@ -1,10 +1,18 @@
-# tests/test_appliedOnHostAction.py
+# tests/test_fingerprint.py
 import json
 from support.assertions import assert_valid_schema
 
 
-def test_get_appliedOnHostAction():
+def test_get_fingerprint0():
     with open('tests/files/test-fingerprint.json') as json_file:
+        json_data = json.load(json_file)
+
+        assert_valid_schema(json_data,
+                            'operations/fingerprint.json.schema')
+
+
+def test_get_fingerprint1():
+    with open('tests/files/test-fingerprint-1.json') as json_file:
         json_data = json.load(json_file)
 
         assert_valid_schema(json_data,
